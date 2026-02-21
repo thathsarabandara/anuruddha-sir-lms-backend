e76d84754e82f95dc4d8ec47df044e57856df421
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-21 16:05:13 +0530

    add database models module for SQLAlchemy model imports

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+"""
+Database models module
+All SQLAlchemy models should be imported here for migration purposes
+"""
+
+from app import db
+
+# Import all models here as they are created
+# from app.models.user import User
+# from app.models.course import Course
+# from app.models.quiz import Quiz
