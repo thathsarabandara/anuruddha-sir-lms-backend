fc6f23f5edd740c3602b645e5bff9c1317135cf9
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 16:24:19 +0530

    style: improve formatting and organization of users module imports

--- app/models/users/__init__.py
+++ app/models/users/__init__.py
@@ -3,16 +3,16 @@ Users Module Models
 Contains all database models for user profiles, preferences, and activity tracking
 """
 
-from app.models.users.user_profile import UserProfile
-from app.models.users.user_preferences import UserPreferences
 from app.models.users.user_activity_log import UserActivityLog
+from app.models.users.user_preferences import UserPreferences
+from app.models.users.user_profile import UserProfile
 from app.models.users.user_statistics import UserStatistics
 from app.models.users.user_suspension_log import UserSuspensionLog
 
 __all__ = [
-    'UserProfile',
-    'UserPreferences',
-    'UserActivityLog',
-    'UserStatistics',
-    'UserSuspensionLog',
+    "UserProfile",
+    "UserPreferences",
+    "UserActivityLog",
+    "UserStatistics",
+    "UserSuspensionLog",
 ]
