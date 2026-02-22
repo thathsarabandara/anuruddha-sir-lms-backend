91c6972fd3f34d594cd6f377de774a8fdbefc222
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 16:17:24 +0530

    style: improve formatting and organization of Certificates module imports

--- app/models/certificates/__init__.py
+++ app/models/certificates/__init__.py
@@ -4,13 +4,13 @@ Manages certificate generation, issuance, verification, and sharing
 """
 
 from .certificate import Certificate
+from .certificate_sharing_log import CertificateSharingLog
 from .certificate_template import CertificateTemplate
 from .certificate_verification_log import CertificateVerificationLog
-from .certificate_sharing_log import CertificateSharingLog
 
 __all__ = [
-    'Certificate',
-    'CertificateTemplate',
-    'CertificateVerificationLog',
-    'CertificateSharingLog'
+    "Certificate",
+    "CertificateTemplate",
+    "CertificateVerificationLog",
+    "CertificateSharingLog",
 ]
