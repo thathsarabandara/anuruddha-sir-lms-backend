d3362eb4912796432b2fbfbfe52ab2f5471898ba
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 13:24:22 +0530

    feat: add __init__.py for Users module to consolidate user-related models

---
+++ app/models/users/__init__.py
@@ -0,0 +1,18 @@
+"""
+Users Module Models
+Contains all database models for user profiles, preferences, and activity tracking
+"""
+
+from app.models.users.user_profile import UserProfile
+from app.models.users.user_preferences import UserPreferences
+from app.models.users.user_activity_log import UserActivityLog
+from app.models.users.user_statistics import UserStatistics
+from app.models.users.user_suspension_log import UserSuspensionLog
+
+__all__ = [
+    'UserProfile',
+    'UserPreferences',
+    'UserActivityLog',
+    'UserStatistics',
+    'UserSuspensionLog',
+]
