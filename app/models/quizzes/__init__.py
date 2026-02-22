f00b1c6ee3815ec29ccccb970e59498ee80d9d85
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 16:21:47 +0530

    style: improve formatting and organization of quizzes module imports

--- app/models/quizzes/__init__.py
+++ app/models/quizzes/__init__.py
@@ -3,18 +3,18 @@ Quizzes Module
 Quizzes, questions, attempts, and grading models
 """
 
-from app.models.quizzes.quiz import Quiz
-from app.models.quizzes.question import Question
-from app.models.quizzes.question_option import QuestionOption
-from app.models.quizzes.quiz_attempt import QuizAttempt
 from app.models.quizzes.attempt_answer import AttemptAnswer
 from app.models.quizzes.manual_grade import ManualGrade
+from app.models.quizzes.question import Question
+from app.models.quizzes.question_option import QuestionOption
+from app.models.quizzes.quiz import Quiz
+from app.models.quizzes.quiz_attempt import QuizAttempt
 
 __all__ = [
-    'Quiz',
-    'Question',
-    'QuestionOption',
-    'QuizAttempt',
-    'AttemptAnswer',
-    'ManualGrade',
+    "Quiz",
+    "Question",
+    "QuestionOption",
+    "QuizAttempt",
+    "AttemptAnswer",
+    "ManualGrade",
 ]
