606b3b8492cd4717ee63c46a2db492529741831c
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 09:25:49 +0530

    add Course model with title and description fields

---
+++ app/models/course.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    
+    def __repr__(self):
+        return f"<Course {self.title}>"
