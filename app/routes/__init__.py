fa31a5201c81b3610255f7bc0d495940ef810516
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 16:25:17 +0530

    style: improve formatting and organization of routes module documentation

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,7 +7,7 @@ Pattern:
     - Routes call services for business logic
     - Routes return JSON responses
     - All logic is in services, not routes
-    
+
 Structure:
     Each feature has a corresponding _routes.py file:
     - health_routes.py: Health check endpoints
@@ -15,7 +15,7 @@ Structure:
     - user_routes.py: User management endpoints (to be created)
     - course_routes.py: Course endpoints (to be created)
     - etc.
-    
+
 Example:
     @bp.route('/endpoint', methods=['GET'])
     def get_endpoint():
