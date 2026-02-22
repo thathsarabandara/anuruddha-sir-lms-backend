8c1c8c4739e3d9317a6659bfb7eb94aebff530bb
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-22 16:20:13 +0530

    style: improve formatting and organization of notifications module imports

--- app/models/notifications/__init__.py
+++ app/models/notifications/__init__.py
@@ -4,17 +4,17 @@ User notifications, preferences, delivery, templates, and batch management
 """
 
 from app.models.notifications.notification import Notification
-from app.models.notifications.notification_preferences import NotificationPreferences
-from app.models.notifications.notification_type_preferences import NotificationTypePreferences
-from app.models.notifications.notification_delivery_log import NotificationDeliveryLog
-from app.models.notifications.notification_template import NotificationTemplate
 from app.models.notifications.notification_batch import NotificationBatch
+from app.models.notifications.notification_delivery_log import NotificationDeliveryLog
+from app.models.notifications.notification_preferences import NotificationPreferences
+from app.models.notifications.notification_template import NotificationTemplate
+from app.models.notifications.notification_type_preferences import NotificationTypePreferences
 
 __all__ = [
-    'Notification',
-    'NotificationPreferences',
-    'NotificationTypePreferences',
-    'NotificationDeliveryLog',
-    'NotificationTemplate',
-    'NotificationBatch',
+    "Notification",
+    "NotificationPreferences",
+    "NotificationTypePreferences",
+    "NotificationDeliveryLog",
+    "NotificationTemplate",
+    "NotificationBatch",
 ]
