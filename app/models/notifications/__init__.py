21f92a19bc7b946e7caba25ca18cde061c7cef50
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-03-03 16:45:40 +0530

    refactor: Remove unused NotificationTemplate import from notifications module

--- app/models/notifications/__init__.py
+++ app/models/notifications/__init__.py
@@ -7,7 +7,6 @@ from app.models.notifications.notification import Notification
 from app.models.notifications.notification_batch import NotificationBatch
 from app.models.notifications.notification_delivery_log import NotificationDeliveryLog
 from app.models.notifications.notification_preferences import NotificationPreferences
-from app.models.notifications.notification_template import NotificationTemplate
 from app.models.notifications.notification_type_preferences import NotificationTypePreferences
 
 __all__ = [
@@ -15,6 +14,5 @@ __all__ = [
     "NotificationPreferences",
     "NotificationTypePreferences",
     "NotificationDeliveryLog",
-    "NotificationTemplate",
     "NotificationBatch",
 ]
